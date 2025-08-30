6cd43e4f4bad00e0282454548220f33fe1166f9d
twkelwin <twkelwin@gmail.com>
2025-08-29 21:23:17 -0300

    Create classes

---
+++ Back-End/dev/src/main/java/com/virtualgym/dev/model/MensalidadeModel.java
@@ -0,0 +1,45 @@
+package com.virtualgym.dev.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "mensalidade")
+public class MensalidadeModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Mensalidade_ID", nullable = false)
+	private Long id;
+	@Column(name = "Estado", nullable = false)
+	private String estado;
+	
+	
+	@Override
+	public String toString() {
+		return "MensalidadeModel [id: " + id + ", estado: " + estado + " ]";
+	}
+	public MensalidadeModel(Long id, String estado) {
+		this.id = id;
+		this.estado = estado;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+
+}
